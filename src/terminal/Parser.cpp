bd4774d0002546570b3d6700960f8b56c9d21714
Christian Parpart <christian@parpart.family>
2019-08-20 00:14:54 +0200

    license update to Apache License 2.0

--- src/terminal/Parser.cpp
+++ src/terminal/Parser.cpp
@@ -1,10 +1,16 @@
-// This file is part of the "libterminal" project, http://github.com/christianparpart/libterminal>
-//   (c) 2018 Christian Parpart <christian@parpart.family>
-//
-// Licensed under the MIT License (the "License"); you may not use this
-// file except in compliance with the License. You may obtain a copy of
-// the License at: http://opensource.org/licenses/MIT
-
+/**
+ * This file is part of the "libterminal" project
+ *   Copyright (c) 2019 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <terminal/ControlCode.h>
 #include <terminal/Parser.h>
 #include <terminal/ParserTables.h>
