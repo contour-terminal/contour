118318572543c2c55baeb16fe70124240ec265e1
Christian Parpart <christian@parpart.family>
2019-08-19 18:13:14 +0200

    Adds license and copyright header

--- src/terminal/Parser.cpp
+++ src/terminal/Parser.cpp
@@ -1,3 +1,10 @@
+// This file is part of the "libterminal" project, http://github.com/christianparpart/libterminal>
+//   (c) 2018 Christian Parpart <christian@parpart.family>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
 #include <terminal/ControlCode.h>
 #include <terminal/Parser.h>
 #include <terminal/ParserTables.h>
--- src/terminal/Screen_test.cpp
+++ src/terminal/Screen_test.cpp
@@ -1,3 +1,10 @@
+// This file is part of the "libterminal" project, http://github.com/christianparpart/libterminal>
+//   (c) 2018 Christian Parpart <christian@parpart.family>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
 #include <terminal/Screen.h>
 #include <catch2/catch.hpp>
 #include <string_view>
