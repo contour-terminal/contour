fcf5f60b02341da9e29c31ba2a680ae959eef0dc
Christian Parpart <christian@parpart.family>
2019-09-02 21:00:58 +0200

    cmake: ensure C++17 is default

--- cmake/Toolchain.cmake
+++ cmake/Toolchain.cmake
@@ -1,3 +1,8 @@
+# setting defaults
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
 if(NOT CMAKE_TOOLCHAIN_FILE)
     set(CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/toolchains/cxx17.cmake"
         CACHE FILEPATH "Path to CMake toolchain file")
