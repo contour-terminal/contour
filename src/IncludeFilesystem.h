1693f61aa2c35e071c5b3bff20b8cbfb8460c909
Stephen <swolter@users.noreply.github.com>
2019-09-27 16:56:51 -0400

    Porting application to OS/X.

--- src/IncludeFilesystem.h
+++ src/IncludeFilesystem.h
@@ -1,17 +1,14 @@
 #pragma once
 
-#if defined(__APPLE__) && defined(__has_include)
-    #if __has_include(<filesystem>)
-        #include <filesystem>
-        namespace FileSystem = std::filesystem;
-    #elif __has_include(<experimental/filesystem>)
-        #include <experimental/filesystem>
-        namespace FileSystem = std::experimental::filesystem;
-    #else
-        #include "boost/filesystem.hpp"
-        namespace FileSystem = boost::filesystem;
-    #endif
-#else
+#if !defined(__has_include) || __has_include(<filesystem>)
+    #include <filesystem>
+    namespace FileSystem = std::filesystem;
+#elif __has_include(<experimental/filesystem>)
+    #include <experimental/filesystem>
+    namespace FileSystem = std::experimental::filesystem;
+#elif __has_include(<boost/filesystem.hpp>)
     #include "boost/filesystem.hpp"
     namespace FileSystem = boost::filesystem;
+#else
+    #error No filesystem implementation found.
 #endif
