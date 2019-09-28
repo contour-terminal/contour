a2ecc7a64e834885e24a3d338265516e8485d9d9
Stephen <swolter@users.noreply.github.com>
2019-09-28 14:47:14 -0400

    Adding in a small check to see if the config file exists and making an empty file if it doesn't

--- src/IncludeFilesystem.h
+++ src/IncludeFilesystem.h
@@ -2,13 +2,18 @@
 
 #if !defined(__has_include) || __has_include(<filesystem>)
     #include <filesystem>
+    #include <system_error>
     namespace FileSystem = std::filesystem;
+    typedef std::error_code FileSystemError;
 #elif __has_include(<experimental/filesystem>)
     #include <experimental/filesystem>
+    #include <system_error>
     namespace FileSystem = std::experimental::filesystem;
+    typedef std::error_code FileSystemError;
 #elif __has_include(<boost/filesystem.hpp>)
     #include "boost/filesystem.hpp"
     namespace FileSystem = boost::filesystem;
+    typedef boost::system::error_code FileSystemError;
 #else
     #error No filesystem implementation found.
 #endif
