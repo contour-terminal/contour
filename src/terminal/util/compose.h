19afd5bd507cac201a616de6f073e71a2625c394
Christian Parpart <christian@parpart.family>
2019-12-16 22:44:36 +0100

    Function composition in C++ :-)

---
+++ src/terminal/util/compose.h
@@ -0,0 +1,55 @@
+/**
+ * This file is part of the "libterminal" project
+ *   Copyright (c) 2019 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+#include <tuple>
+#include <utility>
+
+/**
+ * Provides a facility for function composition.
+ *
+ * Suppose you write code like this:
+ * <code>
+ *   auto result = h(g(f(x)), x);
+ * </code>
+ *
+ * What you can do, and what is much more readable:
+ * <code>
+ *   auto result = f(x) >> compose(g) >> compose(h, x);
+ * </code>
+ *
+ * @param _fun the function to be composed with.
+ * @param _args zero or more function parameters to be curried with the given function.
+ *              The input resulting value of the left-hand-side of the function composition
+ *              will be the last parameter being passed to _fun.
+ *
+ * @return the result of applying all parameters to the composed function.
+ */
+template <typename F, typename... Args>
+constexpr auto compose(F _fun, Args... _args) -> std::pair<F, std::tuple<Args...>>
+{
+    return std::pair{std::move(_fun), std::make_tuple(std::forward<Args>(_args)...)};
+}
+
+/**
+ * Function composition operator, to be used with the compose() function.
+ */
+template<typename S, typename F, typename... Args>
+constexpr auto operator>>(S _input, std::pair<F, std::tuple<Args...>> _chain)
+{
+    return std::apply(
+        _chain.first,
+        std::tuple_cat(std::move(_chain.second), std::tuple<S>{std::move(_input)})
+    );
+}
+
---
+++ src/terminal/util/compose_test.cpp
@@ -0,0 +1,38 @@
+/**
+ * This file is part of the "libterminal" project
+ *   Copyright (c) 2019 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <terminal/util/compose.h>
+#include <catch2/catch.hpp>
+
+TEST_CASE("compose.simple")
+{
+    auto constexpr doubled = [](int v) { return v + v; };
+    auto constexpr squared = [](int v) { return v * v; };
+    auto constexpr a0 = 1;
+    auto constexpr res = a0 >> compose(doubled) >> compose(squared);
+    static_assert(res == 4);
+}
+
+TEST_CASE("compose.withArgs")
+{
+    auto constexpr a0 = 1;
+    auto constexpr a1 = [](int c, int v) { return c + v; };
+    auto constexpr a2 = [](int c1, int c2, int v) { return c1 + c2 + v; };
+    auto constexpr a3 = [](int c1, int c2, int c3, int v) { return c1 + c2 + c3 + v; };
+    auto constexpr res = a0
+        >> compose(a1, 2)
+        >> compose(a2, 3, 4)
+        >> compose(a3, 5, 6, 7);
+    static_assert(28 == res);
+}
+
