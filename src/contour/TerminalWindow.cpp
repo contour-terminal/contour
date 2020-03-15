f49127a82f4f6b596879cb82f28d590a015bdac2
Christian Parpart <christian@parpart.family>
2020-03-15 12:49:53 +0100

    Revive Windows acrylic background blur.

--- src/contour/TerminalWindow.cpp
+++ src/contour/TerminalWindow.cpp
@@ -858,8 +858,66 @@ bool TerminalWindow::enableBackgroundBlur([[maybe_unused]] bool _enable)
     KWindowEffects::enableBlurBehind(winId(), _enable);
     KWindowEffects::enableBackgroundContrast(winId(), !_enable);
     return true;
+#elif defined(_WIN32)
+    // Awesome hack with the noteworty links:
+    // * https://gist.github.com/ethanhs/0e157e4003812e99bf5bc7cb6f73459f (used as code template)
+    // * https://github.com/riverar/sample-win32-acrylicblur/blob/master/MainWindow.xaml.cs
+    // * https://stackoverflow.com/questions/44000217/mimicking-acrylic-in-a-win32-app
+    // p.s.: if you find a more official way to do it, please PR me. :)
+
+    bool success = false;
+    if (HWND hwnd = (HWND)winId(); hwnd != nullptr)
+    {
+        const HINSTANCE hModule = LoadLibrary(TEXT("user32.dll"));
+        if (hModule)
+        {
+           enum WindowCompositionAttribute : int {
+               // ...
+               WCA_ACCENT_POLICY = 19,
+               // ...
+           };
+           enum AcceptState : int {
+               ACCENT_DISABLED = 0,
+               ACCENT_ENABLE_GRADIENT = 1,
+               ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+               ACCENT_ENABLE_BLURBEHIND = 3,
+               ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+               ACCENT_ENABLE_HOSTBACKDROP = 5,
+           };
+            struct ACCENTPOLICY
+            {
+                AcceptState nAccentState;
+                int nFlags;
+                int nColor;
+                int nAnimationId;
+            };
+            struct WINCOMPATTRDATA
+            {
+                WindowCompositionAttribute nAttribute;
+                void const* pData;
+                ULONG ulDataSize;
+            };
+            typedef BOOL(WINAPI *pSetWindowCompositionAttribute)(HWND, WINCOMPATTRDATA const*);
+            const pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(hModule, "SetWindowCompositionAttribute");
+            if (SetWindowCompositionAttribute)
+            {
+               auto const policy = _enable
+                   ? ACCENTPOLICY{ ACCENT_ENABLE_BLURBEHIND, 0, 0, 0 }
+                   : ACCENTPOLICY{ ACCENT_DISABLED, 0, 0, 0 };
+               auto const data = WINCOMPATTRDATA{ WCA_ACCENT_POLICY, &policy, sizeof(ACCENTPOLICY) };
+               BOOL rs = SetWindowCompositionAttribute(hwnd, &data);
+               success = rs != FALSE;
+            }
+            FreeLibrary(hModule);
+        }
+    }
+    return success;
 #else
-    return false;
+   if (_enable)
+       // Get me working on other platforms/compositors (such as OSX, Gnome, ...), please.
+       return false;
+   else
+       return true;
 #endif
 }
 
