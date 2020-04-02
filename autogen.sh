5c883fce830b5a8b31903e96c5ed3e368c68c681
Christian Parpart <christian@parpart.family>
2020-04-02 15:32:36 +0200

    [CMake] autogen.sh: ensure CMAKE_EXPORT_COMPILE_COMMANDS is set to ON

--- autogen.sh
+++ autogen.sh
@@ -14,6 +14,7 @@ cd "${WORKDIR}"
 exec cmake "${ROOTDIR}" \
            -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
            -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" \
+           -DCMAKE_EXPORT_COMPILE_COMMANDS="ON" \
            -DOpenGL_GL_PREFERENCE="LEGACY" \
            -DYAML_CPP_BUILD_CONTRIB="OFF" \
            -DYAML_CPP_BUILD_TOOLS="OFF" \
