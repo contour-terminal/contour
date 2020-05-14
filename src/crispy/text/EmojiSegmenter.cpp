3ccda07f30a51a852d6545a1ba6cde39aa7b79f2
Christian Parpart <christian@parpart.family>
2020-05-14 17:20:57 +0200

    Finally got the emoji segmenter fully functional, with tests.

--- src/crispy/text/EmojiSegmenter.cpp
+++ src/crispy/text/EmojiSegmenter.cpp
@@ -1,16 +1,85 @@
+/**
+ * This file is part of the "libterminal" project
+ *   Copyright (c) 2020 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <crispy/text/EmojiSegmenter.h>
 #include <crispy/text/Unicode.h>
+#include <crispy/escape.h>
+#include <iostream>
 
 namespace crispy::text {
 
-constexpr bool operator<(EmojiSegmentationCategory a, EmojiSegmentationCategory b) noexcept
+enum class EmojiSegmentationCategory
 {
-    return static_cast<int>(a) < static_cast<int>(b);
-}
+    Invalid = -1,
 
-constexpr bool operator>(EmojiSegmentationCategory a, EmojiSegmentationCategory b) noexcept
+    Emoji = 0,
+    EmojiTextPresentation = 1,
+    EmojiEmojiPresentation = 2,
+    EmojiModifierBase = 3,
+    EmojiModifier = 4,
+    EmojiVSBase = 5,
+    RegionalIndicator = 6,
+    KeyCapBase = 7,
+    CombiningEnclosingKeyCap = 8,
+    CombiningEnclosingCircleBackslash = 9,
+    ZWJ = 10,
+    VS15 = 11,
+    VS16 = 12,
+    TagBase = 13,
+    TagSequence = 14,
+    TagTerm = 15,
+};
+
+constexpr inline EmojiSegmentationCategory toCategory(char32_t _codepoint)
 {
-    return static_cast<int>(a) > static_cast<int>(b);
+    auto isEmojiKeycapBase = [](char32_t _codepoint) -> bool {
+        return ('0' <= _codepoint && _codepoint <= '9')
+            || _codepoint == '#' || _codepoint == '*';
+    };
+
+    if (_codepoint == 0x20e3)
+        return EmojiSegmentationCategory::CombiningEnclosingKeyCap;
+    if (_codepoint == 0x20e0)
+        return EmojiSegmentationCategory::CombiningEnclosingCircleBackslash;
+    if (_codepoint == 0x200d)
+        return EmojiSegmentationCategory::ZWJ;
+    if (_codepoint == 0xfe0e)
+        return EmojiSegmentationCategory::VS15;
+    if (_codepoint == 0xfe0f)
+        return EmojiSegmentationCategory::VS16;
+    if (_codepoint == 0x1f3f4)
+        return EmojiSegmentationCategory::TagBase;
+    if ((_codepoint >= 0xE0030 && _codepoint <= 0xE0039) || (_codepoint >= 0xE0061 && _codepoint <= 0xE007A))
+        return EmojiSegmentationCategory::TagSequence;
+    if (_codepoint == 0xE007F)
+        return EmojiSegmentationCategory::TagTerm;
+    if (emoji_modifier_base(_codepoint))
+        return EmojiSegmentationCategory::EmojiModifierBase;
+    if (emoji_modifier(_codepoint))
+        return EmojiSegmentationCategory::EmojiModifier;
+    if (grapheme_cluster_break::regional_indicator(_codepoint))
+        return EmojiSegmentationCategory::RegionalIndicator;
+    if (isEmojiKeycapBase(_codepoint))
+        return EmojiSegmentationCategory::KeyCapBase;
+    if (emoji_presentation(_codepoint))
+        return EmojiSegmentationCategory::EmojiEmojiPresentation;
+    if (emoji(_codepoint) && !emoji_presentation(_codepoint))
+        return EmojiSegmentationCategory::EmojiTextPresentation;
+    if (emoji(_codepoint))
+        return EmojiSegmentationCategory::Emoji;
+
+    return EmojiSegmentationCategory::Invalid;
 }
 
 class RagelIterator {
@@ -29,13 +98,16 @@ class RagelIterator {
         updateCategory();
     }
 
-    constexpr RagelIterator() noexcept : RagelIterator(nullptr, 0, 0) {}
+    constexpr RagelIterator() noexcept : RagelIterator(U"", 0, 0) {}
 
     constexpr char32_t codepoint() const noexcept { return buffer_[cursor_]; }
     constexpr EmojiSegmentationCategory category() const noexcept { return category_; }
     constexpr size_t cursor() const noexcept { return cursor_; }
 
-    constexpr void updateCategory() { category_ = toCategory(codepoint()); }
+    constexpr void updateCategory()
+    {
+        category_ = toCategory(codepoint());
+    }
 
     constexpr int operator*() const noexcept { return static_cast<int>(category_); }
 
@@ -60,12 +132,16 @@ using emoji_text_iter_t = RagelIterator;
 #include "emoji_presentation_scanner.c"
 }
 
-void EmojiSegmenter::scan() noexcept
+void EmojiSegmenter::consume() noexcept
 {
-    auto const i = RagelIterator(begin_, size_, cursor_);
-    auto const e = RagelIterator(begin_, size_, size_);
-    auto const o = scan_emoji_presentation(i, e, &isEmoji_);
-    cursor_ = o.cursor();
+    if (size_ != 0)
+    {
+        lastCursor_ = cursor_;
+        auto const i = RagelIterator(buffer_, size_, cursor_);
+        auto const e = RagelIterator(buffer_, size_, size_);
+        auto const o = scan_emoji_presentation(i, e, &isEmoji_);
+        cursor_ = o.cursor();
+    }
 }
 
 } // end namespace
--- src/crispy/text/EmojiSegmenter.h
+++ src/crispy/text/EmojiSegmenter.h
@@ -18,121 +18,65 @@
 #include <algorithm>
 #include <utility>
 
+#include <fmt/format.h>
+
 namespace crispy::text {
 
-enum class EmojiSegmentationCategory
-{
-    Invalid = -1,
-
-    Emoji = 0,
-    EmojiTextPresentation = 1,
-    EmojiEmojiPresentation = 2,
-    EmojiModifierBase = 3,
-    EmojiModifier = 4,
-    EmojiVSBase = 5,
-    RegionalIndicator = 6,
-    KeyCapBase = 7,
-    CombiningEnclosingKeyCap = 8,
-    CombiningEnclosingCircleBackslash = 9,
-    ZWJ = 10,
-    VS15 = 11,
-    VS16 = 12,
-    TagBase = 13,
-    TagSequence = 14,
-    TagTerm = 15,
-};
-
-constexpr inline EmojiSegmentationCategory toCategory(char32_t _codepoint)
-{
-    auto isEmojiKeycapBase = [](char32_t _codepoint) -> bool {
-        return ('0' <= _codepoint && _codepoint <= '9')
-            || _codepoint == '#' || _codepoint == '*';
-    };
-
-    if (_codepoint == 0x20e3)
-        return EmojiSegmentationCategory::CombiningEnclosingKeyCap;
-    if (_codepoint == 0x20e0)
-        return EmojiSegmentationCategory::CombiningEnclosingCircleBackslash;
-    if (_codepoint == 0x200d)
-        return EmojiSegmentationCategory::ZWJ;
-    if (_codepoint == 0xfe0e)
-        return EmojiSegmentationCategory::VS15;
-    if (_codepoint == 0xfe0f)
-        return EmojiSegmentationCategory::VS16;
-    if (_codepoint == 0x1f3f4)
-        return EmojiSegmentationCategory::TagBase;
-    if ((_codepoint >= 0xE0030 && _codepoint <= 0xE0039) || (_codepoint >= 0xE0061 && _codepoint <= 0xE007A))
-        return EmojiSegmentationCategory::TagSequence;
-    if (_codepoint == 0xE007F)
-        return EmojiSegmentationCategory::TagTerm;
-    if (emoji_modifier_base(_codepoint))
-        return EmojiSegmentationCategory::EmojiModifierBase;
-    if (emoji_modifier(_codepoint))
-        return EmojiSegmentationCategory::EmojiModifier;
-    if (grapheme_cluster_break::regional_indicator(_codepoint))
-        return EmojiSegmentationCategory::RegionalIndicator;
-    if (isEmojiKeycapBase(_codepoint))
-        return EmojiSegmentationCategory::KeyCapBase;
-    if (emoji_presentation(_codepoint))
-        return EmojiSegmentationCategory::EmojiEmojiPresentation;
-    if (emoji(_codepoint) && !emoji_presentation(_codepoint))
-        return EmojiSegmentationCategory::EmojiTextPresentation;
-    if (emoji(_codepoint))
-        return EmojiSegmentationCategory::Emoji;
-
-    return EmojiSegmentationCategory::Invalid;
-}
-
+/**
+ * EmojiSegmenter API for segmenting emojis into text-emoji and emoji-emoji presentations.
+ *
+ * This segmenter is segmenting emojis by their presentation property (text or emoji), that is,
+ * whether an emoji is to be rendered in text mode or in emoji (colored) mode.
+ *
+ * It must be segmenting only emojis and not any other codepoints.
+ */
 class EmojiSegmenter {
   private:
-    char32_t const* begin_;
-    char32_t const* end_;
+    char32_t const* buffer_;
     size_t size_;
+    size_t lastCursor_;
     size_t cursor_;
 
     bool isEmoji_ = false;
-    EmojiSegmentationCategory category_;
 
   public:
-    constexpr EmojiSegmenter(char32_t const* _begin, char32_t const* _end) noexcept
-      : begin_{ _begin },
-        end_{ _end },
-        size_{ static_cast<size_t>(end_ - begin_) },
+    EmojiSegmenter(char32_t const* _buffer, size_t _size) noexcept
+      : buffer_{ _buffer },
+        size_{ _size },
+        lastCursor_{ 0 },
         cursor_{ 0 },
-        isEmoji_{false},
-        category_{ EmojiSegmentationCategory::Invalid }
-    {}
-
-    constexpr EmojiSegmenter(std::u32string_view const& _sv) noexcept
-      : EmojiSegmenter(_sv.begin(), _sv.end())
-    {}
-
-    constexpr EmojiSegmentationCategory category() const noexcept { return category_; }
-
-    constexpr bool isText() const noexcept
+        isEmoji_{false}
     {
-        return category_ == EmojiSegmentationCategory::EmojiTextPresentation;
+        consume();
     }
 
-    constexpr bool isEmoji() const noexcept
-    {
-        return category_ == EmojiSegmentationCategory::EmojiEmojiPresentation;
-    }
+    EmojiSegmenter(std::u32string_view const& _sv) noexcept
+      : EmojiSegmenter(_sv.data(), _sv.size())
+    {}
 
+    /// @returns whether or not the currently segmented emoji is to be rendered in text-presentation or not.
+    constexpr bool isText() const noexcept { return !isEmoji_; }
+
+    /// @returns whether or not the currently segmented emoji is to be rendered in emoji-presentation or not.
+    constexpr bool isEmoji() const noexcept { return isEmoji_; }
+
+    /// @returns the underlying current segment that has been processed the last.
     constexpr std::u32string_view operator*() const noexcept
     {
-        return std::u32string_view(begin_, end_ - begin_);
+        if (cursor_ > 0)
+            return std::u32string_view(buffer_ + lastCursor_, cursor_ - lastCursor_);
+        else
+            return std::u32string_view{};
     }
 
-    void scan() noexcept;
+    void consume() noexcept;
 
-    EmojiSegmenter& operator++() noexcept { scan(); return *this; }
-
+    EmojiSegmenter& operator++() noexcept { consume(); return *this; }
     EmojiSegmenter& operator++(int) noexcept { return ++*this; }
 
     constexpr bool operator==(EmojiSegmenter const& _rhs) const noexcept
     {
-        return begin_ == _rhs.begin_ && end_ == _rhs.end_;
+        return buffer_ == _rhs.buffer_ && size_ == _rhs.size_;
     }
 
     constexpr bool operator!=(EmojiSegmenter const& _rhs) const noexcept { return !(*this == _rhs); }
