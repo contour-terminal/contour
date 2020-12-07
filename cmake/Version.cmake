d3a2665e4ab619a315b01b93fd5244fbd6de75df
Christian Parpart <christian@parpart.family>
2020-12-07 12:33:03 +0100

    [Github] CI: Attempt to fix building on Ubuntu 18.04

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -21,18 +21,26 @@ function(GetVersionInformation VersionTripleVar VersionStringVar)
     if(EXISTS "${CMAKE_SOURCE_DIR}/version.txt")
         file(READ "${CMAKE_SOURCE_DIR}/version.txt" version_text)
         string(STRIP "${version_text}" version_text)
-        string(REGEX MATCH "^v?([0-9]*\.[0-9]+\.[0-9]+).*$" _ ${version_text})
+        string(REGEX MATCH "^v?([0-9]*\\.[0-9]+\\.[0-9]+).*$" _ ${version_text})
         set(THE_VERSION ${CMAKE_MATCH_1})
         set(THE_VERSION_STRING "${version_text}")
-    elseif(EXISTS "${CMAKE_SOURCE_DIR}/.git")
-        execute_process(COMMAND git describe --always --tags --dirty=-modified
+        set(THE_SOURCE "${CMAKE_SOURCE_DIR}/version.txt")
+    endif()
+
+    if(("${THE_VERSION}" STREQUAL "" OR "${THE_VERSION_STRING}" STREQUAL "") AND (EXISTS "${CMAKE_SOURCE_DIR}/.git"))
+        execute_process(COMMAND git describe --always --dirty=-modified --tags
             OUTPUT_VARIABLE version_text
             WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
             OUTPUT_STRIP_TRAILING_WHITESPACE)
-        string(REGEX MATCH "^v?([0-9]*\.[0-9]+\.[0-9]+).*$" _ ${version_text})
-        set(THE_VERSION ${CMAKE_MATCH_1})
-        set(THE_VERSION_STRING "${version_text}")
-    elseif(EXISTS "${CMAKE_SOURCE_DIR}/Changelog.md")
+        string(REGEX MATCH "^v?([0-9]*\\.[0-9]+\\.[0-9]+).*$" _ ${version_text})
+        if(NOT("${CMAKE_MATCH_1}" STREQUAL ""))
+            set(THE_VERSION ${CMAKE_MATCH_1})
+            set(THE_VERSION_STRING "${version_text}")
+            set(THE_SOURCE "git")
+        endif()
+    endif()
+
+    if(("${THE_VERSION}" STREQUAL "" OR "${THE_VERSION_STRING}" STREQUAL "") AND (EXISTS "${CMAKE_SOURCE_DIR}/Changelog.md"))
         file(READ "${CMAKE_SOURCE_DIR}/Changelog.md" changelog_contents)
         # extract and construct version triple
         string(REGEX MATCH "^^### ([0-9]*\.[0-9]+\.[0-9]+).*$" _ "${changelog_contents}")
@@ -44,9 +52,16 @@ function(GetVersionInformation VersionTripleVar VersionStringVar)
         else()
             set(THE_VERSION_STRING "${THE_VERSION}")
         endif()
-    else()
+        set(THE_SOURCE "${CMAKE_SOURCE_DIR}/Changelog.md")
+    endif()
+
+    if("${THE_VERSION}" STREQUAL "" OR "${THE_VERSION_STRING}" STREQUAL "")
         message(FATAL_ERROR "Cannot extract version information.")
     endif()
+
+    message(STATUS "[Version] version source: ${THE_SOURCE}")
+    message(STATUS "[Version] version triple: ${THE_VERSION}")
+    message(STATUS "[Version] version string: ${THE_VERSION_STRING}")
 
     # Write resulting version triple and version string to parent scope's variables.
     set(${VersionTripleVar} "${THE_VERSION}" PARENT_SCOPE)
