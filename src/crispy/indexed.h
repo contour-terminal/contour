bb526a3851f9a7c237efb225ec2ba0c837497363
Christian Parpart <christian@parpart.family>
2020-12-31 07:14:31 +0100

    [crispy] indexed: code refactor and tests added.

--- src/crispy/indexed.h
+++ src/crispy/indexed.h
@@ -1,3 +1,16 @@
+/**
+ * This file is part of the "contour" project.
+ *   Copyright (c) 2020 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include <utility>
@@ -8,11 +21,14 @@ namespace crispy {
 namespace impl { // {{{
     template <typename Container, typename Index>
     struct indexed {
-        Container& container;
-        Index start = 0;
+        using container_type = std::remove_const_t<std::remove_reference_t<Container>>;
 
-        struct iterator {
-            typename Container::iterator iter;
+        Container container;
+        Index start;
+
+        struct iterator { // {{{
+            using inner_type = decltype(std::declval<container_type>().begin());
+            typename container_type::iterator iter;
             Index index = 0;
 
             constexpr iterator& operator++()
@@ -28,14 +44,14 @@ namespace impl { // {{{
                 return *this;
             }
 
-            constexpr auto operator*() const { return std::make_pair(index, *iter); }
+            constexpr auto operator*() const { return std::pair{index, *iter}; }
 
             constexpr bool operator==(const iterator& rhs) const noexcept { return iter == rhs.iter; }
             constexpr bool operator!=(const iterator& rhs) const noexcept { return iter != rhs.iter; }
         };
-
-        struct const_iterator {
-            typename Container::const_iterator iter;
+        // }}}
+        struct const_iterator { // {{{
+            typename container_type::const_iterator iter;
             Index index = 0;
 
             constexpr const_iterator& operator++()
@@ -51,40 +67,28 @@ namespace impl { // {{{
                 return *this;
             }
 
-            constexpr auto operator*() const { return std::make_pair(index, *iter); }
+            constexpr auto operator*() const { return std::make_pair(Index{index}, *iter); }
 
             constexpr bool operator==(const const_iterator& rhs) const noexcept { return iter == rhs.iter; }
             constexpr bool operator!=(const const_iterator& rhs) const noexcept { return iter != rhs.iter; }
         };
+        // }}}
 
-        constexpr auto begin() const
-        {
-            if constexpr (std::is_const<Container>::value)
-                return const_iterator{container.cbegin(), start};
-            else
-                return iterator{container.begin(), start};
-        }
+        constexpr auto begin() const { return const_iterator{container.cbegin(), start}; }
+        constexpr auto end() const { return const_iterator{container.cend()}; }
 
-        constexpr auto end() const
-        {
-            if constexpr (std::is_const<Container>::value)
-                return const_iterator{container.cend()};
-            else
-                return iterator{container.end()};
-        }
+        constexpr auto begin() { return iterator{const_cast<container_type&>(container).begin(), start}; }
+        constexpr auto end() { return iterator{const_cast<container_type&>(container).end()}; }
     };
 } // }}}
 
 template <typename Container, typename Index = std::size_t>
-constexpr auto indexed(Container const& c, Index _start = 0)
+constexpr auto indexed(Container && c, Index _start = 0)
 {
-	return typename std::add_const<impl::indexed<const Container, Index>>::type{c, _start};
-}
-
-template <typename Container, typename Index = std::size_t>
-constexpr auto indexed(Container& c, Index _start = 0)
-{
-	return impl::indexed<Container, Index>{c, _start};
+    if constexpr (std::is_const_v<Container>)
+        return std::add_const_t<impl::indexed<Container, Index>>{std::forward<Container>(c), _start};
+    else
+        return impl::indexed<Container, Index>{std::forward<Container>(c), _start};
 }
 
 } // end namespace
---
+++ src/crispy/indexed_test.cpp
@@ -0,0 +1,105 @@
+/**
+ * This file is part of the "contour" project.
+ *   Copyright (c) 2020 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <crispy/indexed.h>
+
+#include <catch2/catch.hpp>
+#include <fmt/format.h>
+
+#include <array>
+#include <vector>
+
+using namespace crispy;
+
+using std::array;
+using std::vector;
+
+TEST_CASE("indexed.basic", "[indexed]")
+{
+    auto const rng = crispy::indexed(array{'a', 'b', 'c'});
+    auto i = rng.begin();
+    REQUIRE(i.index == 0);
+    REQUIRE(*i.iter == 'a');
+
+    ++i;
+    REQUIRE(i.index == 1);
+    REQUIRE(*i.iter == 'b');
+
+    ++i;
+    REQUIRE(i.index == 2);
+    REQUIRE(*i.iter == 'c');
+
+    ++i;
+    REQUIRE(i == rng.end());
+}
+
+TEST_CASE("indexed.for_loop_basic_lvalue", "[indexed]")
+{
+    size_t k = 0;
+    auto const a = array{'a', 'b', 'c'};
+    for (auto const && [i, c] : crispy::indexed(a))
+    {
+        REQUIRE(i == k);
+        switch (k)
+        {
+            case 0: REQUIRE(c == 'a'); break;
+            case 1: REQUIRE(c == 'b'); break;
+            case 2: REQUIRE(c == 'c'); break;
+            default: REQUIRE(false);
+        }
+        ++k;
+    }
+}
+
+TEST_CASE("indexed.for_loop_basic_rvalue", "[indexed]")
+{
+    size_t k = 0;
+    for (auto const && [i, c] : crispy::indexed(vector{'a', 'b', 'c'}))
+    {
+        REQUIRE(i == k);
+        switch (k)
+        {
+            case 0: REQUIRE(c == 'a'); break;
+            case 1: REQUIRE(c == 'b'); break;
+            case 2: REQUIRE(c == 'c'); break;
+            default: REQUIRE(false);
+        }
+        ++k;
+    }
+}
+
+vector<char> getVec()
+{
+    vector<char> v;
+    v.push_back('a');
+    v.push_back('b');
+    v.push_back('c');
+    return v;
+}
+
+TEST_CASE("indexed.for_loop_basic_rvalue_via_call", "[indexed]")
+{
+    size_t k = 0;
+    for (auto const && [i, c] : crispy::indexed(getVec()))
+    {
+        REQUIRE(i == k);
+        switch (k)
+        {
+            case 0: REQUIRE(c == 'a'); break;
+            case 1: REQUIRE(c == 'b'); break;
+            case 2: REQUIRE(c == 'c'); break;
+            default: REQUIRE(false);
+        }
+        ++k;
+    }
+}
