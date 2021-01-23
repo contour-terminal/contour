f88e98e7848ba6563b0ed2b571c1b260d0c435b7
Christian Parpart <christian@parpart.family>
2021-01-23 14:53:34 +0100

    use custom main in test

--- src/terminal/test_main.cpp
+++ src/terminal/test_main.cpp
@@ -11,5 +11,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define CATCH_CONFIG_MAIN
+// #define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
+
+int main(int argc, char const* argv[])
+{
+    int const result = Catch::Session().run(argc, argv);
+
+    // avoid closing extern console to close on VScode/windows
+    // system("pause");
+
+    return result;
+}
