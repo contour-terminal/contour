83979a858e9e1e5ece1331b497e5429e4afcceac
Christian Parpart <christian@parpart.family>
2021-02-03 07:32:59 +0100

    chrispy: add split

---
+++ src/crispy/split.h
@@ -0,0 +1,39 @@
+/**
+ * This file is part of the "libterminal" project
+ *   Copyright (c) 2019-2020 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <string_view>
+#include <vector>
+
+namespace crispy {
+
+inline auto split(std::string_view _text) -> std::vector<std::string_view>
+{
+    std::vector<std::string_view> output{};
+    size_t a = 0;
+    size_t b = 0;
+    while ((b = _text.find('\n', a)) != std::string_view::npos)
+    {
+        output.emplace_back(_text.substr(a, b - a));
+        a = b + 1;
+    }
+
+    if (a < _text.size())
+        output.emplace_back(_text.substr(a));
+
+    return output;
+}
+
+
+}
