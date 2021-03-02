5f5c5682b722ab9f75e88b86e1ccf4af2aec1b1c
Ave Milia <avemilia@protonmail.com>
2021-03-02 23:19:07 +0100

    [cmake] "unsubmodule" 3rdparty dependencies
    Summary:
    
    * unsubmodule fmtlib, yaml-cpp, libunicode, catch2
    * remove git dependency
    * all: add code to select system versions of libraries
    * fmtlib: behavior change: remove -DFMT_USE_WINDOWS_H=0: fmt no longer
    includes windows.h [1]
    * yaml-cpp: add inline patch to:
        * turn off unneeded compilation targets such as tests
        * silence warnings when compiling contour code
    * yaml-cpp: attempt to download only a single header, attempt failed,
    reverted to downloading full tarball
    * catch2: download only the header [2]
    * all: add dependency on `patch` binary [3][4]
    * all: pin dependencies [5]
    * libunicode: temporary inline patch to make it work with this patch
    
    [1]: <https://github.com/fmtlib/fmt/commit/253d63159f79762ebd40aea0c20704cd29acb3c9>
    
    [2]: compilation times can be further improved by creating separate .cpp
    file and defining main() there
    
    [3]: perhaps temporary, for the lack of other ideas how to patch
    dependencies. A dependency-free way would be to include complete files
    that can be hot-swapped with cmake -E copy. However, this requires
    careful following of patched dependencies development and manual
    backporting "by eye", whereas if patch doesn't apply, it just fails. On
    the other hand, it is just as easy to fetch a `patch` binary from the
    Internet as it is to fetch any tarball dependency. Googling around,
    there is even a native `patch` port for Windows. Not implemented in this
    PR due to lack of working Windows machine to test, left as a TODO in
    code
    
    [4]: another incovenience of `patch` is that you must apply the patch
    exactly once. This is the reason behind guarding it with an if() clause.
    Since the patches are provided verbatim and inline, when developer
    updates the patch, the clause doesn't update, which means the developer
    needs to remember to completely remove the binary dir and cleanly
    reconfigure CMake for the new patch to apply. This can come as
    unexpected, therefore needs to be addressed, so leaving this as another
    TODO for now
    
    [5]: all dependencies (except libunicode) are pinned and
    hashsum-checked. This is good for reproducible builds. If cutting edge
    dependencies are desired often, such functionality can be added later.
    Current implementation does no overly complicated abstraction to support
    all possible cases.
    
    Details:
    
    This patch is aimed towards unbundling contour dependencies from git
    submodules, or in other words, "unsubmoduling" them. This is a desired
    change for a few reasons, as discussed on discord:
    
    * git submodules are _git_ submodules, that is, they require git
    
    * because they require git, tarball archives of code, which github
    provides, are useless -- user needs to populate the submodules manually,
    which requires git. And guess who downloads tarballs? Definitely not the
    people who know how to use git and have it installed.
    
    * even when you clone the repo with git, you need to clone recursively,
    or if you didn't read the docs/forgot, you first need to realize that
    this the actual problem (since the error message will be bogus), and
    then you need to google "git sumbmodule populate", because who the hell
    remembers how to do it. This is unpleasant experience from the get-go.
    It can be fixed in CMake by probing and populating submodules at
    configure time. But it's still inferior to the idea of having no hard
    dependency on git.
    
    Therefore, this patch attemtps to replace dependency management via
    submodules functionality on best-effort basis.
    
    One such limitation is having a hard dependency on CMake 3.10, which
    does not have FetchContent module (CMake 3.11). FetchContent does
    exactly what this patch tries to do, which is: download external
    dependencies and make them available to the rest of the build at
    configure time. As a workaround, ExternalProject_Add is used in a tricky
    way.
    
    Tests:
    
    * tested with all pinned dependencies
    * tested with all system dependencies (except libunicode) on Manjaro
    Linux (Arch Linux derivative)

---
+++ cmake/3rdparty.cmake
@@ -0,0 +1,209 @@
+macro(contour_add_fmtlib)
+    # To download fmtlib at configure time we need a small trick: because
+    # ExternalProject runs at build time, we create a fake minimal project and
+    # build it. This will download and extract the sources.
+    set(fmtlib_download_dir "${3rdparty_prefix}/fmtlib/fmtlib-download")
+    set(fmtlib_source_dir "${3rdparty_prefix}/fmtlib/fmtlib-src")
+    set(fmtlib_binary_dir "${3rdparty_prefix}/fmtlib/fmtlib-build")
+    set(fmtlib-externalproject-in "${fmtlib_download_dir}/fmtlib-externalproject.cmake.in")
+    if(NOT EXISTS "${fmtlib-externalproject-in}")
+        file(WRITE "${fmtlib-externalproject-in}" [[
+project(fmtlib-externalproject NONE)
+include(ExternalProject)
+ExternalProject_Add(fmtlib
+        URL http://github.com/fmtlib/fmt/archive/${CONTOUR_EMBEDDED_FMTLIB_VERSION}.tar.gz
+        URL_MD5 ${CONTOUR_EMBEDDED_FMTLIB_MD5SUM}
+        SOURCE_DIR "${fmtlib_source_dir}"
+        BINARY_DIR "${fmtlib_binary_dir}"
+        UPDATE_DISCONNECTED 1
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        TEST_COMMAND "")
+]])
+    endif()
+    configure_file("${fmtlib-externalproject-in}" "${fmtlib_download_dir}/CMakeLists.txt")
+    execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+            WORKING_DIRECTORY "${fmtlib_download_dir}"
+            RESULT_VARIABLE result OUTPUT_VARIABLE output ERROR_VARIABLE output)
+    if(result)
+        message(FATAL_ERROR "'${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} .' failed:\n${output}")
+    endif()
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+            WORKING_DIRECTORY "${fmtlib_download_dir}"
+            RESULT_VARIABLE result OUTPUT_VARIABLE output ERROR_VARIABLE output)
+    if(result)
+        message(FATAL_ERROR "'${CMAKE_COMMAND} --build .' failed:\n${output}")
+    endif()
+
+    add_subdirectory("${fmtlib_source_dir}" "${fmtlib_binary_dir}" EXCLUDE_FROM_ALL)
+endmacro()
+
+macro(contour_add_yaml_cpp)
+    # Same trick as in fmtlib.
+    set(yaml-cpp_download_dir "${3rdparty_prefix}/yaml-cpp/yaml-cpp-download")
+    set(yaml-cpp_source_dir "${3rdparty_prefix}/yaml-cpp/yaml-cpp-src")
+    set(yaml-cpp_binary_dir "${3rdparty_prefix}/yaml-cpp/yaml-cpp-build")
+    set(yaml-cpp-externalproject-in "${yaml-cpp_download_dir}/yaml-cpp-externalproject.cmake.in")
+    if(NOT EXISTS "${yaml-cpp-externalproject-in}")
+        file(WRITE "${yaml-cpp-externalproject-in}" [[
+project(yaml-cpp-externalproject NONE)
+include(ExternalProject)
+ExternalProject_Add(yaml-cpp
+        URL http://github.com/jbeder/yaml-cpp/archive/yaml-cpp-${CONTOUR_EMBEDDED_YAML_CPP_VERSION}.tar.gz
+        URL_MD5 ${CONTOUR_EMBEDDED_YAML_CPP_MD5SUM}
+        SOURCE_DIR "${yaml-cpp_source_dir}"
+        BINARY_DIR "${yaml-cpp_binary_dir}"
+        UPDATE_DISCONNECTED 1
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        TEST_COMMAND "")
+]])
+    endif()
+
+    configure_file("${yaml-cpp-externalproject-in}" "${yaml-cpp_download_dir}/CMakeLists.txt")
+    execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+                    WORKING_DIRECTORY "${yaml-cpp_download_dir}"
+                    RESULT_VARIABLE result OUTPUT_VARIABLE output ERROR_VARIABLE output)
+    if(result)
+        message(FATAL_ERROR "'${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} .' failed:\n${output}")
+    endif()
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+                    WORKING_DIRECTORY "${yaml-cpp_download_dir}"
+                    RESULT_VARIABLE result OUTPUT_VARIABLE output ERROR_VARIABLE output)
+    if(result)
+        message(FATAL_ERROR "'${CMAKE_COMMAND} --build .' failed:\n${output}")
+    endif()
+
+    # Patch it: don't produce warnings on yaml-cpp files and don't build tests.
+    set(yaml_cpp_patch "${3rdparty_prefix}/patches/yaml_cpp.patch")
+    if(NOT EXISTS "${yaml_cpp_patch}")
+        file(WRITE "${yaml_cpp_patch}" [[
+--- CMakeLists.txt	2021-03-03 08:33:57.271688830 +0100
++++ CMakeLists.txt.new	2021-03-03 09:32:34.817113397 +0100
+@@ -15,8 +15,8 @@
+ ### Project options
+ ###
+ ## Project stuff
+-option(YAML_CPP_BUILD_TESTS "Enable testing" ON)
+-option(YAML_CPP_BUILD_TOOLS "Enable parse tools" ON)
++option(YAML_CPP_BUILD_TESTS "Enable testing" OFF)
++option(YAML_CPP_BUILD_TOOLS "Enable parse tools" OFF)
+ option(YAML_CPP_BUILD_CONTRIB "Enable contrib stuff in library" ON)
+ option(YAML_CPP_INSTALL "Enable generation of install target" ON)
+
+@@ -259,7 +259,7 @@
+ endif()
+
+ if (NOT CMAKE_VERSION VERSION_LESS 2.8.12)
+-    target_include_directories(yaml-cpp
++  target_include_directories(yaml-cpp SYSTEM
+         PUBLIC $<BUILD_INTERFACE:${YAML_CPP_SOURCE_DIR}/include>
+                $<INSTALL_INTERFACE:${INCLUDE_INSTALL_ROOT_DIR}>
+         PRIVATE $<BUILD_INTERFACE:${YAML_CPP_SOURCE_DIR}/src>)
+]])
+        if(NOT CONTOUR_PATCH_PROGRAM)
+            find_program(CONTOUR_PATCH_PROGRAM patch DOC "Patch program") # todo: version, windows
+            if(CONTOUR_PATCH_PROGRAM_NOTFOUND)
+                message(FATAL_ERROR "Patch program is required")
+            endif()
+        endif()
+        execute_process(COMMAND "${CONTOUR_PATCH_PROGRAM}" -i "${yaml_cpp_patch}"
+                WORKING_DIRECTORY "${yaml-cpp_source_dir}"
+                RESULT_VARIABLE result OUTPUT_VARIABLE output ERROR_VARIABLE output)
+        if(result)
+            message(FATAL_ERROR "'${CONTOUR_PATCH_PROGRAM} -i <${yaml_cpp_patch}' failed:\n${output}")
+        endif()
+    endif()
+    add_subdirectory("${yaml-cpp_source_dir}" "${yaml-cpp_binary_dir}" EXCLUDE_FROM_ALL)
+    add_library(yaml-cpp::yaml-cpp ALIAS yaml-cpp)
+endmacro()
+
+macro(contour_add_libunicode)
+    # Same trick as in fmtlib.
+    set(libunicode_download_dir "${3rdparty_prefix}/libunicode/libunicode-download")
+    set(libunicode_source_dir "${3rdparty_prefix}/libunicode/libunicode-src")
+    set(libunicode_binary_dir "${3rdparty_prefix}/libunicode/libunicode-build")
+    set(libunicode-externalproject-in "${libunicode_download_dir}/libunicode-externalproject.cmake.in")
+    if(NOT EXISTS "${libunicode-externalproject-in}")
+        file(WRITE "${libunicode-externalproject-in}" [[
+project(libunicode-externalproject NONE)
+include(ExternalProject)
+ExternalProject_Add(libunicode
+        URL http://github.com/christianparpart/libunicode/tarball/${CONTOUR_EMBEDDED_LIBUNICODE_VERSION}
+        # SKIP URL_MD5
+        SOURCE_DIR "${libunicode_source_dir}"
+        BINARY_DIR "${libunicode_binary_dir}"
+        UPDATE_DISCONNECTED 1
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        TEST_COMMAND "")
+]])
+    endif()
+    configure_file("${libunicode-externalproject-in}" "${libunicode_download_dir}/CMakeLists.txt")
+    execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+            WORKING_DIRECTORY "${libunicode_download_dir}"
+            RESULT_VARIABLE result OUTPUT_VARIABLE output ERROR_VARIABLE output)
+    if(result)
+        message(FATAL_ERROR "'${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} .' failed:\n${output}")
+    endif()
+    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+            WORKING_DIRECTORY "${libunicode_download_dir}"
+            RESULT_VARIABLE result OUTPUT_VARIABLE output ERROR_VARIABLE output)
+    if(result)
+        message(FATAL_ERROR "'${CMAKE_COMMAND} --build .' failed:\n${output}")
+    endif()
+
+    # XXX: temporary patch until libunicode gets rid of sumbodules.
+    set(libunicode_patch "${3rdparty_prefix}/patches/libunicode.patch")
+    if(NOT EXISTS "${libunicode_patch}")
+        file(WRITE "${libunicode_patch}" [[
+--- CMakeLists.txt	2021-03-03 08:40:11.184332244 +0100
++++ CMakeLists.txt.new	2021-03-03 08:49:06.336734764 +0100
+@@ -48,18 +48,6 @@
+ # ----------------------------------------------------------------------------
+ # 3rdparty dependencies
+
+-if(LIBUNICODE_EMBEDDED_FMTLIB)
+-    add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/fmt" EXCLUDE_FROM_ALL)
+-    add_definitions(-DFMT_USE_WINDOWS_H=0)
+-else()
+-    # master project must provide its own fmtlib
+-endif()
+-
+-if(LIBUNICODE_TESTING AND LIBUNICODE_EMBEDDED_CATCH2)
+-    add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/3rdparty/catch2")
+-else()
+-    # master project must provide its own fmtlib
+-endif()
+
+ add_subdirectory(src/unicode)
+ add_subdirectory(src/tools)
+]])
+        if(NOT CONTOUR_PATCH_PROGRAM)
+            find_program(CONTOUR_PATCH_PROGRAM patch DOC "Patch program")
+            if(CONTOUR_PATCH_PROGRAM_NOTFOUND)
+                message(FATAL_ERROR "Patch program is required")
+            endif()
+        endif()
+        execute_process(COMMAND "${CONTOUR_PATCH_PROGRAM}" -i "${libunicode_patch}"
+                WORKING_DIRECTORY "${libunicode_source_dir}"
+                RESULT_VARIABLE result OUTPUT_VARIABLE output ERROR_VARIABLE output)
+        if(result)
+            message(FATAL_ERROR "'${CONTOUR_PATCH_PROGRAM} -i ${libunicode_patch}' failed:\n${output}")
+        endif()
+    endif()
+    add_subdirectory("${libunicode_source_dir}" "${libunicode_binary_dir}" EXCLUDE_FROM_ALL)
+endmacro()
+
+macro(contour_add_catch2)
+    file(DOWNLOAD http://raw.githubusercontent.com/catchorg/Catch2/v${CONTOUR_EMBEDDED_CATCH2_VERSION}/single_include/catch2/catch.hpp
+            "${3rdparty_prefix}/catch2/catch.hpp"
+            #SHOW_PROGRESS
+            EXPECTED_MD5 ${CONTOUR_EMBEDDED_CATCH2_MD5SUM})
+    add_library(Catch2 INTERFACE)
+    target_include_directories(Catch2 SYSTEM INTERFACE "${3rdparty_prefix}")
+    add_library(Catch2::Catch2 ALIAS Catch2)
+endmacro()
