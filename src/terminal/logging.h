cd03cfaf394c196198c718922039cec83a694470
Christian Parpart <christian@parpart.family>
2021-03-21 16:45:45 +0100

    [cmake] have VT raw/trace logging enabled by default.

---
+++ src/terminal/logging.h
@@ -0,0 +1,31 @@
+/**
+ * This file is part of the "libterminal" project
+ *   Copyright (c) 2019-2020 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <crispy/debuglog.h>
+
+namespace terminal {
+
+auto const inline KeyboardTag = crispy::debugtag::make("vt.input", "Logs terminal input events.");
+auto const inline VTParserTag = crispy::debugtag::make("vt.parser", "Logs terminal parser errors.");
+
+#if defined(LIBTERMINAL_LOG_RAW)
+auto const inline ScreenRawOutputTag = crispy::debugtag::make("vt.output", "Logs raw writes to the terminal screen.");
+#endif
+
+#if defined(LIBTERMINAL_LOG_TRACE)
+auto const inline VTParserTraceTag = crispy::debugtag::make("vt.trace", "Logs terminal parser instruction trace.");
+#endif
+
+}
