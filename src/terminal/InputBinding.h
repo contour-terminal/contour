7f932444e7f4b3cb8269f9405baf04a80d109b20
Christian Parpart <christian@parpart.family>
2021-07-09 08:34:06 +0200

    [terminal] InputBinding: typo-fix

--- src/terminal/InputBinding.h
+++ src/terminal/InputBinding.h
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#pragma aonce
+#pragma once
 
 #include <terminal/MatchModes.h>
 #include <terminal/InputGenerator.h>
