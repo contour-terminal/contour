fa556e27e25f3373a1a1a394de31c3a047f11cac
Christian Parpart <christian@parpart.family>
2021-11-20 00:37:35 +0100

    CI: Adds notcurses-demo via Xvfb to test chain & reduce test train minutes.

---
+++ .github/workflows/Xvfb-contour-run.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+set -x
+
+export LIBGL_ALWAYS_SOFTWARE='true'
+
+if [[ ! ~/.terminfo ]]
+then
+    ln -sf ./build/src/contour/terminfo ~/.terminfo
+fi
+
+DISPLAY=:99
+
+Xvfb $DISPLAY -screen 0 1280x1024x24 &
+XVFB_PID=$!
+
+sleep 3
+
+contour terminal \
+        debug pty,gui.session,gui.display \
+        display ${DISPLAY} \
+        early-exit-threshold 0 \
+        dump-state-at-exit \
+        ${@}
+
+# ~/opt/notcurses/bin/notcurses-demo -p ~/opt/notcurses/share/notcurses
+
+echo "::set-output name=exitCode::$?"
+
+kill "${XVFB_PID}"
+
