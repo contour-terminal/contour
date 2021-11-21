f49344774f30953d45347eec3e58bed396831fae
data-man <dataman@tutanota.com>
2021-11-22 04:29:30 +0500

    Update all 3rd-party libraries: - use the latest GSL - update all 3rd-party libs

--- cmake/ThirdParties.cmake
+++ cmake/ThirdParties.cmake
@@ -1,14 +1,15 @@
 include(CPM)
 
-set(3rdparty_catch2_version "22750cde0e0d2c02ab447e2500422f2711356053" CACHE STRING "catch2: commit hash")
-set(3rdparty_fmt_version "3b6e409cd8573f63e4acad7717d9082bd898ec87" CACHE STRING "fmt: commit hash")
-set(3rdparty_freetype_version "bbab0a675ef29b0eaa96054a69ada63fc3051c51" CACHE STRING "freetype: commit hash")
-set(3rdparty_harfbuzz_version "d58f1685cee7fef904f4149561a2dcf5ec519ac3" CACHE STRING "harfbuzz: commit hash")
+set(3rdparty_catch2_version "2db1cf34047f76cb2679a3804b476881536ad27b" CACHE STRING "catch2: commit hash")
+set(3rdparty_fmt_version "19cac63fe4b4d8fe6a4ced28de16a68659cf9035" CACHE STRING "fmt: commit hash")
+set(3rdparty_freetype_version "81912a1385e8fc7694eda820221e15745cdcada4" CACHE STRING "freetype: commit hash")
+set(3rdparty_gsl_version "e0880931ae5885eb988d1a8a57acf8bc2b8dacda" CACHE STRING "GSL: commit hash")
+set(3rdparty_harfbuzz_version "84dc4e85e889d4b24cca7bb8ef04563fc6d1c3e6" CACHE STRING "harfbuzz: commit hash")
 set(3rdparty_libunicode_version "3c59a3a0eb0c57c3081d48ea77c22809ac7c2d6e" CACHE STRING "libunicode: commit hash")
-set(3rdparty_mimalloc_version "v2.0.2" CACHE STRING "mimalloc: release tag")
-set(3rdparty_range_v3_version "8f690283cc03146ad20514741cf69eafb325e974" CACHE STRING "range_v3: commit hash")
-set(3rdparty_yaml_cpp_version "1713859b054b0a7fd867a59905dfbb0d3f774d54" CACHE STRING "yaml-cpp: commit hash")
-set(3rdparty_termbenchpro_version "513cc8a7eb15b3b1a7940816944056d168d2c22a" CACHE STRING "termbench-pro: commit hash")
+set(3rdparty_mimalloc_version "v2.0.3" CACHE STRING "mimalloc: release tag")
+set(3rdparty_range_v3_version "83783f578e0e6666d68a3bf17b0038a80e62530e" CACHE STRING "range_v3: commit hash")
+set(3rdparty_yaml_cpp_version "328d2d85e833be7cb5a0ab246cc3f5d7e16fc67a" CACHE STRING "yaml-cpp: commit hash")
+set(3rdparty_termbenchpro_version "5a79261fbf5d26c9bf9d9a0d31f22ef4556cdd3b" CACHE STRING "termbench-pro: commit hash")
 
 if(CONTOUR_TESTING OR CRISPY_TESTING OR LIBTERMINAL_TESTING)
   set(CATCH_BUILD_EXAMPLES OFF CACHE INTERNAL "")
@@ -22,7 +23,7 @@ if(CONTOUR_TESTING OR CRISPY_TESTING OR LIBTERMINAL_TESTING)
     NAME catch2
     VERSION ${3rdparty_catch2_version}
     URL https://github.com/catchorg/Catch2/archive/${3rdparty_catch2_version}.zip
-    URL_HASH SHA256=ab21f8c8804f3c9987d21a731440611d8dfecf8a80efcd2e46bf67a3d9bdf967
+    URL_HASH SHA256=2ab73811ae7e931f7014a0619cf2947a6937eaa8794a4b44bd29acc5b1ed67eb
     EXCLUDE_FROM_ALL YES
   )
 endif()
@@ -31,7 +32,7 @@ CPMAddPackage(
   NAME fmt
   VERSION ${3rdparty_fmt_version}
   URL https://github.com/fmtlib/fmt/archive/${3rdparty_fmt_version}.zip
-  URL_HASH SHA256=d5a6f6e3431fec6332021dc69a14dd11b85b0103b6317d33b2e5e5a91fd08add
+  URL_HASH SHA256=a84fe9643527312fad9a1eef94dd6ebf2358d13c030f90457c3c25ebcf62b702
   EXCLUDE_FROM_ALL YES
 )
 
@@ -39,7 +40,7 @@ CPMAddPackage(
   NAME range_v3
   VERSION ${3rdparty_range_v3_version}
   URL https://github.com/ericniebler/range-v3/archive/${3rdparty_range_v3_version}.zip
-  URL_HASH SHA256=d866cb8f56aea491928af5cb1e7d2dbc4d46bd8a1a2a7b88194fb42b2c126e4b
+  URL_HASH SHA256=eb976c9cf3ac4ae3a1c0ebd37e3a0230c73e92ec030e7f2a18b0e1597c70291c
   EXCLUDE_FROM_ALL YES
 )
 
@@ -52,15 +53,15 @@ CPMAddPackage(
   NAME yaml_cpp
   VERSION ${3rdparty_yaml_cpp_version}
   URL https://github.com/jbeder/yaml-cpp/archive/${3rdparty_yaml_cpp_version}.zip
-  URL_HASH SHA256=086e25ffc95eb54deb949f3e3343daff1722007375947da3d36b7ebd89e9ea97
+  URL_HASH SHA256=b6b314ad5a5095536d0066dc5f9c4dd041039071b34cc7163f6d63b6ad81d023
   EXCLUDE_FROM_ALL YES
 )
 
 CPMAddPackage(
   NAME GSL
-  VERSION "3.1.0"
-  URL https://github.com/microsoft/GSL/archive/refs/tags/v3.1.0.zip
-  URL_HASH SHA256=a1041e41e60f9cb3789036f1c84ea9b4298823cbe94d16b096971fdc3de485b7
+  VERSION ${3rdparty_gsl_version}
+  URL https://github.com/microsoft/GSL/archive/${3rdparty_gsl_version}.zip
+  URL_HASH SHA256=4ece9bdccbd140d8fcd8df471f6da4c49d8a26ff0bca4ff329683995b3cce274
   EXCLUDE_FROM_ALL YES
 )
 
@@ -68,7 +69,7 @@ CPMAddPackage(
   NAME termbenchpro
   VERSION ${3rdparty_termbenchpro_version}
   URL https://github.com/contour-terminal/termbench-pro/archive/${3rdparty_termbenchpro_version}.zip
-  URL_HASH SHA256=555d6dd4e3017e93cc3d6a0b0507e0e1a8eb4534d636ce6bf30ead2d8c0a9e0c
+  URL_HASH SHA256=6c5f044e7738a560c70794dbcf61e3011be4514f43bda55962084f81c7a20270
   EXCLUDE_FROM_ALL YES
 )
 
@@ -87,7 +88,7 @@ if(CONTOUR_BUILD_WITH_MIMALLOC)
     NAME mimalloc
     VERSION ${3rdparty_mimalloc_version}
     URL https://github.com/microsoft/mimalloc/archive/${3rdparty_mimalloc_version}.zip
-    URL_HASH SHA256=6ccba822e251b8d10f8a63d5d7767bc0cbfae689756a4047cdf3d1e4a9fd33d0
+    URL_HASH SHA256=8e5f0b74fdafab09e8853415700a9ade4d62d5f56cd43f54adf02580ceda86c1
     EXCLUDE_FROM_ALL YES
   )
 endif()
@@ -97,13 +98,13 @@ if(CONTOUR_BUILD_WITH_EMBEDDED_FT_HB)
     NAME harfbuzz
     VERSION ${3rdparty_harfbuzz_version}
     URL https://github.com/harfbuzz/harfbuzz/archive/${3rdparty_harfbuzz_version}.zip
-    URL_HASH SHA256=cfff8c1703536f61b0c90f82ae1ca3e62e43bdf99c07504a3d107fd8ec808cbc
+    URL_HASH SHA256=b94f2c95b347fca0414892a79022d9a12e684ddbc65529c57a9cba4e2d8d3a42
   )
 
   CPMAddPackage(
     NAME freetype
     VERSION ${3rdparty_freetype_version}
     URL https://github.com/freetype/freetype/archive/${3rdparty_freetype_version}.zip
-    URL_HASH SHA256=d3f4d2ed447d09263eaf0fb1dccedee67fb75664b28b4b70c72c50b6c5d2223e
+    URL_HASH SHA256=9c02e5a4309260288a2cba071af8fbe1cf430a5c6d3caa5a8ab973e5e2a83d90
   )
 endif()
