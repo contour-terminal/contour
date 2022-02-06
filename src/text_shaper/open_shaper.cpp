1364ec488def8a0494503b9879b370b3669e81f9
Christian Parpart <christian@parpart.family>
2022-02-07 00:26:58 +0100

    [text_shaper] Quickly fixing severe brain-damage implications.

--- src/text_shaper/open_shaper.cpp
+++ src/text_shaper/open_shaper.cpp
@@ -649,7 +649,9 @@ struct open_shaper::Private // {{{
         font_metrics output {};
 
         output.line_height = scaleVertical(ftFace, ftFace->height);
-        output.advance = max(computeAverageAdvance(ftFace), int(double(output.line_height) * 2.0 / 3.0));
+        output.advance = computeAverageAdvance(ftFace);
+        if (!output.advance)
+            output.advance = int(double(output.line_height) * 2.0 / 3.0);
         output.ascender = scaleVertical(ftFace, ftFace->ascender);
         output.descender = scaleVertical(ftFace, ftFace->descender);
         output.underline_position = scaleVertical(ftFace, ftFace->underline_position);
