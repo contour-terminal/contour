96249c283ea6fa69a415eecbe4a25b96f056830d
Christian Parpart <christian@parpart.family>
2022-02-24 12:23:52 +0100

    [contour] Blur.{h,cpp} fixes

--- src/contour/opengl/Blur.h
+++ src/contour/opengl/Blur.h
@@ -1,3 +1,16 @@
+/**
+ * This file is part of the "libterminal" project
+ *   Copyright (c) 2019-2021 Christian Parpart <christian@parpart.family>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include <contour/opengl/Vertex.h>
@@ -6,14 +19,25 @@
 #include <QtCore/QElapsedTimer>
 #include <QtCore/QtMath>
 #include <QtGui/QOffscreenSurface>
-#include <QtGui/QOpenGLBuffer>
-#include <QtGui/QOpenGLContext>
-#include <QtGui/QOpenGLFramebufferObject>
-#include <QtGui/QOpenGLFunctions_3_3_Core>
-#include <QtGui/QOpenGLShaderProgram>
-#include <QtGui/QOpenGLTexture>
-#include <QtGui/QOpenGLVertexArrayObject>
 #include <QtGui/QVector2D>
+
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    #include <QtOpenGL/QOpenGLBuffer>
+    #include <QtOpenGL/QOpenGLContext>
+    #include <QtOpenGL/QOpenGLFramebufferObject>
+    #include <QtOpenGL/QOpenGLFunctions_3_3_Core>
+    #include <QtOpenGL/QOpenGLShaderProgram>
+    #include <QtOpenGL/QOpenGLTexture>
+    #include <QtOpenGL/QOpenGLVertexArrayObject>
+#else
+    #include <QtGui/QOpenGLBuffer>
+    #include <QtGui/QOpenGLContext>
+    #include <QtGui/QOpenGLFramebufferObject>
+    #include <QtGui/QOpenGLFunctions_3_3_Core>
+    #include <QtGui/QOpenGLShaderProgram>
+    #include <QtGui/QOpenGLTexture>
+    #include <QtGui/QOpenGLVertexArrayObject>
+#endif
 
 namespace contour::opengl
 {
