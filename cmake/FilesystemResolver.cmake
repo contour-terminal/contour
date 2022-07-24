a50acc67521dd1a3f28ad523c05d5328835a496f
Christian Parpart <christian@parpart.family>
2022-07-24 21:45:32 +0200

    Applying some tweaks with regards to Flatpak and AppStream (XML) recommendations.

--- cmake/FilesystemResolver.cmake
+++ cmake/FilesystemResolver.cmake
@@ -12,7 +12,10 @@ else()
     set(USING_BOOST_FILESYSTEM FALSE PARENT_SCOPE)
     set(USING_BOOST_FILESYSTEM FALSE)
     message(STATUS "[FilesystemResolver]: Using standard C++ filesystem API")
-    if(APPLE OR "${CMAKE_SYSTEM}" MATCHES "Linux")
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION MATCHES "8.")
+        # With respect to the supported operating systems, especially Ubuntu 18.04 Linux with GCC 8 needs this.
+        # Newer C++ compilers won't need that but still compile if ithe respective linker flag is provided.
+        # TODO: Remove this check once Ubuntu 18.04 LTS won't bee needed anymore.
         set(FILESYSTEM_LIBS "stdc++fs")
     else()
         set(FILESYSTEM_LIBS)
