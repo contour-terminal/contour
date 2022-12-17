ae14ffbe69ed7732ecfe95d3a7db7959582ef547
Christian Parpart <christian@parpart.family>
2022-12-17 22:19:11 +0100

    [vtbackend] ColorPalette: Trying to work around a compiler bug on some GCC versions, so it seems.

--- src/vtbackend/ColorPalette.h
+++ src/vtbackend/ColorPalette.h
@@ -76,55 +76,9 @@ struct ColorPalette
     /// TODO: And even the naming sounds wrong. Better would be makeIndexedColorsBrightForBoldText or similar.
     bool useBrightColors = false;
 
-    Palette palette = []() constexpr {
-        Palette colors;
+    static Palette const defaultColorPalette;
 
-        // normal colors
-        colors[0] = 0x000000_rgb; // black
-        colors[1] = 0xa00000_rgb; // red
-        colors[2] = 0x00a000_rgb; // green
-        colors[3] = 0xa0a000_rgb; // yellow
-        colors[4] = 0x0000a0_rgb; // blue
-        colors[5] = 0xa000a0_rgb; // magenta
-        colors[6] = 0x00a0a0_rgb; // cyan
-        colors[7] = 0xc0c0c0_rgb; // white
-
-        // bright colors
-        colors[8] = 0x707070_rgb;  // bright black (dark gray)
-        colors[9] = 0xff0000_rgb;  // bright red
-        colors[10] = 0x00ff00_rgb; // bright green
-        colors[11] = 0xffff00_rgb; // bright yellow
-        colors[12] = 0x0000ff_rgb; // bright blue
-        colors[13] = 0xff00ff_rgb; // bright magenta
-        colors[14] = 0x00ffff_rgb; // bright blue
-        colors[15] = 0xffffff_rgb; // bright white
-
-        // colors 16-231 are a 6x6x6 color cube
-        for (unsigned red = 0; red < 6; ++red)
-            for (unsigned green = 0; green < 6; ++green)
-                for (unsigned blue = 0; blue < 6; ++blue)
-                    colors[16 + (red * 36) + (green * 6) + blue] =
-                        RGBColor { static_cast<uint8_t>(red ? (red * 40 + 55) : 0),
-                                   static_cast<uint8_t>(green ? (green * 40 + 55) : 0),
-                                   static_cast<uint8_t>(blue ? (blue * 40 + 55) : 0) };
-
-        // colors 232-255 are a grayscale ramp, intentionally leaving out black and white
-        for (uint8_t gray = 0, level = uint8_t(gray * 10 + 8); gray < 24;
-             ++gray, level = uint8_t(gray * 10 + 8))
-            colors[size_t(232 + gray)] = RGBColor { level, level, level };
-
-        // dim colors
-        colors[256 + 0] = 0x000000_rgb; // black
-        colors[256 + 1] = 0xa00000_rgb; // red
-        colors[256 + 2] = 0x008000_rgb; // green
-        colors[256 + 3] = 0x808000_rgb; // yellow
-        colors[256 + 4] = 0x000080_rgb; // blue
-        colors[256 + 5] = 0x800080_rgb; // magenta
-        colors[256 + 6] = 0x008080_rgb; // cyan
-        colors[256 + 7] = 0x808080_rgb; // white
-
-        return colors;
-    }();
+    Palette palette = defaultColorPalette;
 
     [[nodiscard]] RGBColor normalColor(size_t _index) const noexcept
     {
