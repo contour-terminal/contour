5076ef10e0796b46e767a6bc6f5bf7e9bdf7d5e5
Christian Parpart <christian@parpart.family>
2023-01-31 20:53:26 +0100

    [CMake] EnableCcache.cmake: Add ENABLE_CCACHE option
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- cmake/EnableCcache.cmake
+++ cmake/EnableCcache.cmake
@@ -2,16 +2,21 @@
 #
 # The ccache is auto-enabled if the tool is found.
 # To disable set -DCCACHE=OFF option.
-if(NOT DEFINED CMAKE_CXX_COMPILER_LAUNCHER)
-    find_program(CCACHE ccache DOC "ccache tool path; set to OFF to disable")
-    if(CCACHE)
-        set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
-        if(COMMAND cotire)
-            # Change ccache config to meet cotire requirements.
-            set(ENV{CCACHE_SLOPPINESS} pch_defines,time_macros)
+option(ENABLE_CCACHE "Enable compiler cache (ccache) [default: ON]" ON)
+if(ENABLE_CCACHE)
+    if(NOT DEFINED CMAKE_CXX_COMPILER_LAUNCHER)
+        find_program(CCACHE ccache DOC "ccache tool path; set to OFF to disable")
+        if(CCACHE)
+            set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+            if(COMMAND cotire)
+                # Change ccache config to meet cotire requirements.
+                set(ENV{CCACHE_SLOPPINESS} pch_defines,time_macros)
+            endif()
+            message(STATUS "[ccache] Enabled: ${CCACHE}")
+        else()
+            message(STATUS "[ccache] Disabled: Not found.")
         endif()
-        message(STATUS "[ccache] Enabled: ${CCACHE}")
-    else()
-        message(STATUS "[ccache] Disabled.")
     endif()
+else()
+    message(STATUS "[ccache] Disabled.")
 endif()
