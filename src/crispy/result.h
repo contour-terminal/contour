8c9a325b1d46f292c3916d2af001f5163aa2cc61
Christian Parpart <christian@parpart.family>
2023-11-14 18:18:12 +0100

    [crispy] Improve result<T, E> type, add monadic functions.
    Signed-off-by: Christian Parpart <christian@parpart.family>

--- src/crispy/result.h
+++ src/crispy/result.h
@@ -7,6 +7,15 @@
 namespace crispy
 {
 
+template <typename T>
+struct failure
+{
+    T value;
+};
+
+template <typename T>
+failure(T) -> failure<T>;
+
 template <typename T, typename E = std::error_code>
 class result
 {
@@ -19,7 +28,7 @@ class result
     {
     }
 
-    constexpr result(error_type error) noexcept(std::is_nothrow_move_constructible_v<error_type>):
+    constexpr result(failure<error_type> error) noexcept(std::is_nothrow_move_constructible_v<error_type>):
         _data(std::move(error))
     {
     }
@@ -32,28 +41,72 @@ class result
 
     ~result() noexcept = default;
 
+    constexpr void emplace(value_type value) noexcept(std::is_nothrow_move_constructible_v<value_type>)
+    {
+        _data = std::move(value);
+    }
+
     // clang-format off
 
-    constexpr explicit operator bool() const noexcept { return std::holds_alternative<value_type>(_data); }
-    constexpr bool operator !() const noexcept { return std::holds_alternative<error_type>(_data); }
+    [[nodiscard]] constexpr explicit operator bool() const noexcept { return std::holds_alternative<value_type>(_data); }
+    [[nodiscard]] constexpr bool operator !() const noexcept { return std::holds_alternative<failure<error_type>>(_data); }
 
-    [[nodiscard]] constexpr bool good() const noexcept { return std::holds_alternative<value_type>(_data); }
-    [[nodiscard]] constexpr bool is_error() const noexcept { return std::holds_alternative<error_type>(_data); }
+    [[nodiscard]] constexpr value_type& operator*() & noexcept { return std::get<value_type>(_data); }
+    [[nodiscard]] constexpr value_type const& operator*() const& noexcept { return std::get<value_type>(_data); }
+    [[nodiscard]] constexpr value_type operator*() && noexcept { return std::move(std::get<value_type>(_data)); }
+
+    [[nodiscard]] constexpr bool has_value() const noexcept { return std::holds_alternative<value_type>(_data); }
+    [[nodiscard]] constexpr bool is_error() const noexcept { return std::holds_alternative<failure<error_type>>(_data); }
 
     [[nodiscard]] constexpr value_type& value() & noexcept { return std::get<value_type>(_data); }
     [[nodiscard]] constexpr value_type const& value() const& noexcept { return std::get<value_type>(_data); }
     [[nodiscard]] constexpr value_type&& value() && noexcept { return std::move(std::get<value_type>(_data)); }
     [[nodiscard]] constexpr value_type const&& value() const&& noexcept { return std::move(std::get<value_type>(_data)); }
 
-    [[nodiscard]] constexpr error_type& error() & noexcept { return std::get<error_type>(_data); }
-    [[nodiscard]] constexpr error_type const& error() const& noexcept { return std::get<error_type>(_data); }
-    [[nodiscard]] constexpr error_type&& error() && noexcept { return std::move(std::get<error_type>(_data)); }
-    [[nodiscard]] constexpr error_type const&& error() const&& noexcept { return std::move(std::get<error_type>(_data)); }
+    [[nodiscard]] constexpr error_type& error() & noexcept { return std::get<failure<error_type>>(_data).value; }
+    [[nodiscard]] constexpr error_type const& error() const& noexcept { return std::get<failure<error_type>>(_data).value; }
+    [[nodiscard]] constexpr error_type&& error() && noexcept { return std::move(std::get<failure<error_type>>(_data).value); }
+    [[nodiscard]] constexpr error_type const&& error() const&& noexcept { return std::move(std::get<failure<error_type>>(_data).value); }
 
     // clang-format on
 
+    [[nodiscard]] constexpr value_type value_or(value_type alternative) noexcept(
+        std::is_nothrow_move_constructible_v<value_type>)
+    {
+        return std::holds_alternative<value_type>(_data) ? std::get<value_type>(_data)
+                                                         : std::move(alternative);
+    }
+
+    constexpr auto transform(auto&& continuation) const
+        noexcept(noexcept(continuation(std::declval<value_type>())))
+    // NB: Declaring the return type does break MSVC
+    // -> result<decltype(continuation(std::declval<value_type>())), error_type>
+    {
+        using new_value_type = decltype(continuation(std::declval<value_type>()));
+        using new_result_type = result<new_value_type, error_type>;
+
+        if (has_value())
+            return new_result_type { continuation(value()) };
+        else
+            return new_result_type { failure { error() } };
+    }
+
+    constexpr auto transform_error(auto&& continuation) const
+        noexcept(noexcept(continuation(std::declval<error_type>())))
+    // NB: Declaring the return type does break MSVC
+    // -> result<value_type, decltype(continuation(std::declval<error_type>()))>
+    {
+        using new_error_type = decltype(continuation(std::declval<error_type>()));
+        using new_result_type = result<value_type, new_error_type>;
+
+        if (has_value())
+            return new_result_type { value() };
+        else
+            return new_result_type { failure { continuation(error()) } };
+    }
+
   private:
-    std::variant<value_type, error_type> _data;
+    std::variant<value_type, failure<error_type>> _data;
 };
 
 } // namespace crispy
