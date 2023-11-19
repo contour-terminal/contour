0ab31f012ff10de949b233294b34186f6f1bafc2
Christian Parpart <christian@parpart.family>
2023-11-19 14:37:15 +0100

    Add CMake files to .editorconfig

--- cmake/CIncludeMe.cmake
+++ cmake/CIncludeMe.cmake
@@ -1,13 +1,13 @@
 add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/cincludeme)
 
 function(CIncludeMe INPUT_FILE OUTPUT_FILE SYMBOL_NAME NAMESPACE)
-	add_custom_command(
-		OUTPUT "${OUTPUT_FILE}"
-		WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    add_custom_command(
+        OUTPUT "${OUTPUT_FILE}"
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
         COMMAND cincludeme "${OUTPUT_FILE}" "${NAMESPACE}" "${INPUT_FILE}" "${SYMBOL_NAME}"
         DEPENDS cincludeme "${INPUT_FILE}"
-		COMMENT "Generating ${OUTPUT_FILE} from ${INPUT_FILE}"
-		VERBATIM
-	)
-	set_source_files_properties("${OUTPUT_FILE}" PROPERTIES GENERATED TRUE)
+        COMMENT "Generating ${OUTPUT_FILE} from ${INPUT_FILE}"
+        VERBATIM
+    )
+    set_source_files_properties("${OUTPUT_FILE}" PROPERTIES GENERATED TRUE)
 endfunction()
