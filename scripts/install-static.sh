3144166616b32a6e2689b60156b680bfc460a680
Yaraslau Tamashevich <yaraslau.tamashevich@gmail.com>
2024-06-24 12:23:04 +0300

    Reduce number of dependencies

--- scripts/install-static.sh
+++ scripts/install-static.sh
@@ -12,15 +12,25 @@ configure_install() {
     saved_dir=$(pwd)
     cd $1
     shift 1
-    if [ -f "autogen.sh" ]; then
-        sh autogen.sh
-    fi
-
     ./configure $@
     make -j
     make install
     cd $saved_dir
 }
+
+autogen_install() {
+    saved_dir=$(pwd)
+    cd $1
+    shift 1
+    if [ -f autogen.sh ]; then
+        ./autogen.sh
+    fi
+    ./configure $@
+    make -j
+    make install
+    cd $saved_dir
+}
+
 
 cmake_install(){
     saved_dir=$(pwd)
@@ -42,11 +52,24 @@ meson_install(){
     cd $saved_dir
 }
 
+func_install_package(){
+    func_install=$1
+    url=$2
+    fetch $url
+    shift 2
+    $func_install $(basename $url .tar.gz) $@
+}
+
 configure_install_package() {
     url=$1
     shift 1
-    fetch $url
-    configure_install $(basename $url .tar.gz) $@
+    func_install_package configure_install $url $@ --enable-static --disable-shared
+}
+
+autogen_install_package() {
+    url=$1
+    shift 1
+    func_install_package autogen_install $url $@ --enable-static --disable-shared
 }
 
 cmake_install_package() {
@@ -64,13 +87,16 @@ meson_install_package() {
     meson_install $(basename $url .tar.xz) $@
 }
 
+
+# install libpng
+configure_install_package "http://ftp-osl.osuosl.org/pub/libpng/src/libpng16/libpng-1.6.34.tar.gz" --enable-maintainer-mode
 # install libb2
-configure_install_package "https://github.com/BLAKE2/libb2/releases/download/v0.98.1/libb2-0.98.1.tar.gz" --disable-dependency-tracking --enable-static --disable-shared
+autogen_install_package "https://github.com/BLAKE2/libb2/releases/download/v0.98.1/libb2-0.98.1.tar.gz" --disable-dependency-tracking
+# install pcre
+autogen_install_package "https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz"
 # install pcre2
-configure_install_package "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.44/pcre2-10.44.tar.gz" --enable-pcre2-16  --enable-static --disable-shared
+autogen_install_package "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.44/pcre2-10.44.tar.gz" --enable-pcre2-16
 # install double-conversion
 cmake_install_package "double-conversion-3.3.0" "https://github.com/google/double-conversion/archive/refs/tags/v3.3.0.tar.gz"
-# install libgraphite2
-cmake_install_package "graphite2-1.3.14" "https://github.com/silnrsi/graphite/releases/download/1.3.14/graphite2-1.3.14.tgz"
-# install harfbuzz
-cmake_install_package "harfbuzz-8.5.0" "https://github.com/harfbuzz/harfbuzz/releases/download/8.5.0/harfbuzz-8.5.0.tar.xz"
+# install libX11
+configure_install_package "https://www.x.org/releases/individual/lib/libX11-1.8.9.tar.gz"
